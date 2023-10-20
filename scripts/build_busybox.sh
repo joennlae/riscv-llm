0ece4b68fbad3b189cf9c02f9f11942a6c89ee7e
Jannis Schönleber <joennlae@gmail.com>
2023-10-20 12:21:49 +0200

    chore: cleanup

--- scripts/build_busybox.sh
+++
@@ -1,7 +0,0 @@
-# building busybox
-
-CROSS_COMPILE="riscv64-unknown-linux-gnu-"
-
-cd deps/busybox
-make defconfig
-make -j $(nproc)
--- scripts/build_linux.sh
+++
@@ -1,8 +0,0 @@
-# build linux for riscv
-
-cd deps/linux
-export ARCH=riscv
-export CROSS_COMPILE=riscv64-buildroot-linux-gnu-
-export CFLAGS="-march=rv64imafd_zicsr_zifencei"
-make defconfig
-make
