cf0e7bfb554c15bb995166db8e1acc997ec8eba3
Jannis Schönleber <joennlae@gmail.com>
2023-10-20 23:00:01 +0200

    ci: add necessary backslashes to make it work

--- test/src/llama.cpp/test.mk
+++ test/src/llama.cpp/test.mk
@@ -9,7 +9,7 @@ test-llama:
 # download Llama 7B chat model Q4_K_M https://huggingface.co/TheBloke/Llama-2-7b-Chat-GGUF
 	$(RUN) "cd llama.cpp/models/ && wget https://huggingface.co/TheBloke/Llama-2-7b-Chat-GGUF/resolve/main/llama-2-7b-chat.Q4_K_M.gguf"
 # run an inference on RISC-V
-	$(RUN) "cd llama.cpp && ./main -m models/llama-2-7b-chat.Q4_K_M.gguf -p "What do you think about RISC-V?" -n 100 -e"
+	$(RUN) "cd llama.cpp && ./main -m models/llama-2-7b-chat.Q4_K_M.gguf -p \"What do you think about RISC-V?\" -n 100 -e"
 
 test-llama-clean:
 	$(RUN) "rm -rf llama.cpp"
